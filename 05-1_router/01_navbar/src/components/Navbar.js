59e0eab402044ea54cde06c87db828be0c8fcda3
kimdh42 <kimdh1992@gmail.com>
2024-06-03 17:52:32 +0900

    React

---
+++ 05-1_router/01_navbar/src/components/Navbar.js
@@ -0,0 +1,26 @@
+import {NavLink} from 'react-router-dom';
+
+function Navbar() {
+
+    const activeStyle = {
+        backgroundColor : 'orangered'
+    };
+
+    return(
+        <div>
+            <ul>
+                <li>
+                    <NavLink to="/" style={({isActive}) => isActive ? activeStyle : undefined}>HOME</NavLink>
+                </li>
+                <li>
+                    <NavLink to="/about" style={({isActive}) => isActive ? activeStyle : undefined}>ABOUT</NavLink>
+                </li>
+                <li>
+                    <NavLink to="/menu" style={({isActive}) => isActive ? activeStyle : undefined}>MENU</NavLink>
+                </li>
+            </ul>
+        </div>
+    );
+}
+
+export default Navbar;
